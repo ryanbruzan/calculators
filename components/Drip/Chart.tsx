689887ab2e7621e5079ef855b7d1c3e7c99c5f42
Ryan Bruzan <ryanbruzan220@gmail.com>
2024-02-15 09:15:53 -0500

    feat: chart tweaks

--- components/Drip/Chart.tsx
+++ components/Drip/Chart.tsx
@@ -1,22 +1,63 @@
-import { Line, LineChart, ResponsiveContainer, XAxis, YAxis } from 'recharts';
+import formatMoney from '@/utils/formatMoney';
+import {
+	Line,
+	LineChart,
+	ResponsiveContainer,
+	Tooltip,
+	TooltipProps,
+	XAxis,
+} from 'recharts';
+import styles from './drip.module.scss';
 import { Year } from './types';
 
 type Props = {
 	data: Year[];
 };
 
+const CustomTooltip = ({
+	active,
+	payload,
+}: TooltipProps<Year['end'], string>) => {
+	if (active && payload && payload.length) {
+		const point: Year = payload[0].payload;
+		return (
+			<div className="chart-tooltip">
+				<p>End of Year {point.year}</p>
+				<div className="line">
+					<div className="label">Age</div>
+					<div className="value">{point.age + 1}</div>
+				</div>
+				<div className="line">
+					<div className="label">Balance</div>
+					<div className="value">{formatMoney(point.end)}</div>
+				</div>
+				<div className="line">
+					<div className="label">Salary</div>
+					<div className="value">{formatMoney(point.salary)}/yr</div>
+				</div>
+			</div>
+		);
+	}
+
+	return null;
+};
+
 const Chart = ({ data }: Props) => {
 	return (
-		<div style={{ width: '100%', aspectRatio: 16 / 9, maxHeight: 300 }}>
+		<div className={styles.chart}>
 			<ResponsiveContainer>
 				<LineChart data={data}>
 					<Line
 						type="monotone"
 						dataKey="end"
 						stroke="#fff"
+						strokeWidth={1}
 					/>
-					<XAxis dataKey="year" />
-					<YAxis />
+					<XAxis
+						dataKey="year"
+						strokeDasharray="3 3"
+					/>
+					<Tooltip content={CustomTooltip} />
 				</LineChart>
 			</ResponsiveContainer>
 		</div>
